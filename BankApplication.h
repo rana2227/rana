4cbb29d518d82a20ea5975fbb176d2ae7d0c5ec6
rana2227 <44726377+rana2227@users.noreply.github.com>
2018-11-17 18:51:50 +0200

    Add files via upload

---
+++ BankApplication.h
@@ -0,0 +1,14 @@
+#ifndef BANKAPPLICATION_H
+#define BANKAPPLICATION_H
+
+
+class BankApplication
+{
+    public:
+        BankApplication();
+        virtual ~BankApplication();
+    protected:
+    private:
+};
+
+#endif // BANKAPPLICATION_H
